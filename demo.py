e4c1799393df46f97abffb23d51ad5f995b4a82f
Yice Zhang <zzh19971968@foxmail.com>
2018-05-09 16:02:50 +0800

    不重要的修改

--- demo.py
+++ demo.py
@@ -1,6 +1,7 @@
 import random
 import itertools
 import numpy as np
+from collections import defaultdict
 from metric_learn import LMNN,NCA,ITML_Supervised
 
 from sklearn.model_selection import KFold,StratifiedKFold
@@ -37,7 +38,7 @@ def make_metric_data(X, y):
 
 
 class Simple_Version:
-	def __init__(self,n_feature, n_hidden=4, net_num=10):
+	def __init__(self,n_feature, n_hidden=12, net_num=10):
 		net_gene = lambda :torch.nn.Sequential(
 				torch.nn.Linear(n_feature, n_hidden),
 				torch.nn.ReLU(),
@@ -55,7 +56,10 @@ class Simple_Version:
 			_X = torch.FloatTensor(_X)
 			_y = torch.FloatTensor(_y)
 			_X, _y = Variable(_X),Variable(_y)
+			print ('\n-->')
 			for t in range(2000):
+				if t%1000 == 0:
+					print (t,end=" ")
 				prediction = net(_X)
 				loss = self.loss_func(prediction, _y)
 				optimizer.zero_grad()
@@ -72,10 +76,20 @@ class Simple_Version:
 		return d
 
 def load_data():
-	iris_data = load_iris()
+	iris_data = load_digits () #load_iris()
 	X = iris_data['data']
 	y = iris_data['target']
-	return X,y
+	new_X = []
+	new_y = []
+	dic = defaultdict(int)
+	for img,label in zip(X,y):
+		if dic[label]>=10:
+			continue
+		new_X.append(img)
+		new_y.append(label)
+		dic[label]+=1
+	print (new_y,len(new_y))
+	return np.array(new_X),np.array(new_y)
 
 def knn_method(X, y, splits=6):
 	skf = StratifiedKFold(n_splits=splits)
@@ -88,7 +102,9 @@ def knn_method(X, y, splits=6):
 		knn.fit(X_train, y_train)
 		y_pred = knn.predict(X_test)
 		a = sum(y_pred == y_test)/len(y_test)
-		# print (a)
+		print (a)
+		print (y_pred)
+		print (y_test)
 		accuracy += a
 	return (accuracy/splits)
 
@@ -96,7 +112,7 @@ def knn_method(X, y, splits=6):
 def lmnn_method(X, y, splits=6):
 	skf = StratifiedKFold(n_splits=splits)
 	accuracy = 0
-	lmnn = LMNN(k=3, learn_rate=1e-6)
+	lmnn = LMNN(k=10, learn_rate=1e-6)
 	for train_index, test_index in skf.split(X, y):
 		X_train, X_test = X[train_index], X[test_index]
 		y_train, y_test = y[train_index], y[test_index]
@@ -109,6 +125,8 @@ def lmnn_method(X, y, splits=6):
 		knn.fit(X_train, y_train)
 		y_pred = knn.predict(X_test)
 		a = sum(y_pred == y_test)/len(y_test)
+
+		print ('\t>>',a)
 		accuracy += a
 	return (accuracy/splits)
 
@@ -119,9 +137,10 @@ def our_method(X, y, splits=6):
 	for train_index, test_index in skf.split(X, y):
 		X_train, X_test = X[train_index], X[test_index]
 		y_train, y_test = y[train_index], y[test_index]
-		sv = Simple_Version(8)	
+		sv = Simple_Version(64*2)	
 		sv.fit(X_train, y_train)
 
+		print ('度量学习完毕')
 		knn = neighbors.KNeighborsClassifier(metric=sv.distance)
 		knn.fit(X_train, y_train)
 		y_pred = knn.predict(X_test)
@@ -133,7 +152,8 @@ def our_method(X, y, splits=6):
 
 if __name__ == "__main__":
 	X,y = load_data()
-	fs = (knn_method,lmnn_method,our_method)
+	# fs = (knn_method,lmnn_method,our_method)
+	fs = (knn_method,)
 	for f in fs:
 		accuracy=f(X,y,6)
 		print (accuracy)
